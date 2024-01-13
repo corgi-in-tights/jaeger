c6af6b2e4300afee2f9150425044c539e6a9afad
corgi-in-tights <reyaan.chitale@gmail.com>
2024-01-13 13:03:00 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-export FLASK_APP=./api/index.py
+docker build -t my-redis-image .
+docker run -d --name my-redis-container -p 6379:6379 my-redis-image
 
-pipenv run flask --debug run -h 0.0.0.0
+pipenv run python3 src/manage.py
